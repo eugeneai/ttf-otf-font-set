ced4b08d0038392b1d526130e69807f56006a124
Evgeny Cherkashin <eugene@irnok.net>
2016-08-24 09:51:11 +0800

    Bugs corrected.

--- update-db.sh
+++ update-db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export OSFONTDIR="/usr/local/share/fonts;/usr/share/fonts;$HOME/.fonts"
+export OSFONTDIR="$HOME/.fonts;/usr/local/share/fonts;/usr/share/fonts"
 
 mtxrun --script fonts --reload
 
 mkluatexfontdb --force --verbose=-1 -vvv
+
+mtxrun --script fonts --list --all --pattern=* > list-installed.txt
--- update.sh
+++ update.sh
@@ -11,7 +11,13 @@ do
     mkdir -p $dir
     tmp="$tmparch.$ext"
     wget $url -O $tmp
-    7z e -y -o$dir -r $tmp *.otf *.ttf README* readme* LICEN* licen*
+    if [ "$ext" == "zip" ]; then
+        7z e -y -o$dir -r $tmp
+    else
+        7z e -y -o/tmp -r $tmp
+        7z e -y -r -o$dir ${tmp::-3} *.otf *.ttf README* readme* LICEN* licen*
+        rm -f ${tmp::-3}
+    fi
     rm -f $tmp
 done < font.list
 
@@ -37,19 +43,22 @@ popd
 pkgver=1.7.27
 wget "http://source.winehq.org/source/fonts/tahoma.ttf?!v=wine-$pkgver&_raw=1" -O CoreFonts/tahoma.ttf
 wget "http://source.winehq.org/source/fonts/tahomabd.ttf?!v=wine-$pkgver&_raw=1" -O CoreFonts/tahomabd.ttf
-wget "http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe" -O /tmp/ppt-vew.exe
+
+wget "http://download.microsoft.com/download/E/6/7/E675FFFC-2A6D-4AB0-B3EB-27C9F8C8F696/PowerPointViewer.exe" -O /tmp/ppt-view.exe
 mkdir -p VistaFonts
 pushd /tmp
 7z x -y ppt-view.exe ppviewer.cab
 popd
-7z e -y -oVistaFonts /tmp/ppviewer1.cab *.ttc *.TTC *.ttf *.TTF *.OTF *.otf
+7z e -y -oVistaFonts /tmp/ppviewer.cab *.ttc *.TTC *.ttf *.TTF *.OTF *.otf
 pushd VistaFonts/
 for f in `find -depth`; do mv -f ${f} ${f,,} ; done
 FONTFORGE_LANGUAGE=ff fontforge -c 'Open("cambria.ttc"); Generate("cambria.ttf")'
 FONTFORGE_LANGUAGE=ff fontforge -c 'Open("meiryo.ttc"); Generate("meiryo.ttf")'
 FONTFORGE_LANGUAGE=ff fontforge -c 'Open("meiryob.ttc"); Generate("meiryob.ttf")'
 popd
-rm -f /tmp/ppt-vew.exe
-rm -f /tmp/ppviewer*.cab
+rm -f /tmp/ppt-view.exe
+rm -f /tmp/ppviewer.cab
 
 ./update-db.sh
+
+
